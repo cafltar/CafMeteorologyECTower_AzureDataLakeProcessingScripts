2bf845937df0411cbb3466276e421b1ee550d76f
Bryan Carlson <bryan.r.carlson@gmail.com>
2021-05-18 15:23:53 -0700

    Added loop for processing flux and met data

--- src/LTARCAFTowerReport.py
+++ src/LTARCAFTowerReport.py
@@ -13,9 +13,14 @@ import AzureDataLakeAccess as ADLA
 import pathlib
 
 # Change only these things; are the controls to the script that are not within the set-up excel file
-Time = '30T' # Timestamp of the datafile
-col = 'Flux' # Column for the different paths between Flux or Met
+#Time = '30T' # Timestamp of the datafile
+#col = 'Flux' # Column for the different paths between Flux or Met
 #tag = '_AGCY2021Update.csv' # End tag of the file to be saved; added to the end of CEF varaible below
+
+flux = {'col': 'Flux', 'Time': '30T'}
+met = {'col': 'Met', 'Time': '15T'}
+
+DataTables = [flux, met]
 #*********************************************************************
 save = True # If want to save the aggregated file or not; default is True
 #Sites = ['CookEast','CookWest','BoydNorth', 'BoydSouth'] # Name of the sites wanted; can be as many as want but must be within square brackets
@@ -41,29 +46,34 @@ outputPath.mkdir(parents=True, exist_ok=True)
 
 
 #%% Download and aggregate the files from Azure blob storage
-for k in range (0,len(Sites)):
-    # Different file structure and output locations for the different sites
-    access = pd.read_excel(configPath, sheet_name =Sites[k],index_col = 'Variable')
+for dataTable in DataTables:
 
-    # Add path information to access
-    access[col]["inputPath"] = str(inputPath)
-    access[col]["workingPath"] = str(workingPath)
-    access[col]["outputPath"] = str(outputPath)
+    col = dataTable['col']
+    Time = dataTable['Time']
 
-    # Directory should be where the base file starts. There needs to be some start file even if it is blank with the date of the start point; 
-    # I haven't sorted out a "first" pass without a start file to be used. 
-    colT = col + '_V'+S_V[k]
-    #CEF = 'C:\\Users\\russe\\Desktop\\LTAR\\Problems\\Temp\\Aggregate\\'+Sites[k]+'*_'+colT+'*.csv' 
-    #globString = Sites[k]+'*_'+colT+'*.csv'
+    for k in range (0,len(Sites)):
+        # Different file structure and output locations for the different sites
+        access = pd.read_excel(configPath, sheet_name =Sites[k],index_col = 'Variable')
 
-    # {Site}\{Site}_{Met/Flux}_AggregateQC_CY{YYYY}_V{ProgramSignature}_{YYYYMMDD}.csv
-    globString = Sites[k] + "\\" + Sites[k]+'_'+col+'_AggregateQC_CY*'+'_V'+S_V[k]+'*.csv'
-    CEF = str(outputPath / globString)
+        # Add path information to access
+        access[col]["inputPath"] = str(inputPath)
+        access[col]["workingPath"] = str(workingPath)
+        access[col]["outputPath"] = str(outputPath)
 
-    # Calls the function that access the Azure data lake using the options given in the first section. 
-    # Can add the save and date options if want them to be different than the default
-    df = ADLA.AccessAzure(Sites[k], col, Time, access, CEF, QC=True)
-    #df = ADLA.AccessAzure(Sites[k], col, Time, access, CEF,tag, QC=True, startDate="2021-01-27")
+        # Directory should be where the base file starts. There needs to be some start file even if it is blank with the date of the start point; 
+        # I haven't sorted out a "first" pass without a start file to be used. 
+        colT = col + '_V'+S_V[k]
+        #CEF = 'C:\\Users\\russe\\Desktop\\LTAR\\Problems\\Temp\\Aggregate\\'+Sites[k]+'*_'+colT+'*.csv' 
+        #globString = Sites[k]+'*_'+colT+'*.csv'
+
+        # {Site}\{Site}_{Met/Flux}_AggregateQC_CY{YYYY}_V{ProgramSignature}_{YYYYMMDD}.csv
+        globString = Sites[k] + "\\" + Sites[k]+'_'+col+'_AggregateQC_CY*'+'_V'+S_V[k]+'*.csv'
+        CEF = str(outputPath / globString)
+
+        # Calls the function that access the Azure data lake using the options given in the first section. 
+        # Can add the save and date options if want them to be different than the default
+        df = ADLA.AccessAzure(Sites[k], col, Time, access, CEF, QC=True)
+        #df = ADLA.AccessAzure(Sites[k], col, Time, access, CEF,tag, QC=True, startDate="2021-01-27")
 #if col =='Flux':
 #    TRP.TowerReport()
 # if col == 'Met':
